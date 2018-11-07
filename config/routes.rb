31fb1cb05c4a05859c07240d1bf2f3d2323631d6
remka <bolist@mail.ru>
2018-11-07 07:53:15 +0200

    creat menu 4 pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@
 Rails.application.routes.draw do
   root 'posts#index', as: 'home'
 
-  get 'about' => 'pages#about'
-  get 'contacts' => 'pages#contacts'
+  get 'about' => 'pages#about', as: 'about'
+  get 'contacts' => 'pages#contacts', as:'contacts'
 
   resources :posts
 end
