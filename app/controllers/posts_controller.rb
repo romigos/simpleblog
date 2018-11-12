d30b5a6c92533e51a6b3b039e6cf5739e67c3d6f
remka <bolist@mail.ru>
2018-11-12 23:28:09 +0200

    git commit -a -m "edit and delete posts"

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,24 @@ def show
   @post = Post.find(params[:id])
 end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if (@post.update(post_params))
+      redirect_to @post
+    else render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
   def create
     # render plaine: params[:post].inspect
   @post = Post.new(post_params)
